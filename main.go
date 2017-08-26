8aef5723ec19a4123d7c3f1b262d4000c314406d
Divan Visagie <visagiedivan@gmail.com>
2017-08-26 10:54:30 +0200

    Update the way things are extracted

--- main.go
+++ main.go
@@ -1,88 +1,23 @@
 package main
 
 import (
-	"archive/zip"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
+
+	"github.com/mholt/archiver"
 )
 
-func getDownloadUrl(userUrl string) string {
+func getDownloadURL(userURL string) string {
 	// https://codeload.github.com/divanvisagie/postl/zip/master
-	return fmt.Sprintf("https://codeload.github.com/%s/zip/master", userUrl)
-}
-
-// https://golangcode.com/unzip-files-in-go/
-func Unzip(src string, dest string) ([]string, error) {
-
-	var filenames []string
-
-	r, err := zip.OpenReader(src)
-	if err != nil {
-		return filenames, err
-	}
-	defer r.Close()
-
-	for _, f := range r.File {
-
-		rc, err := f.Open()
-		if err != nil {
-			return filenames, err
-		}
-		defer rc.Close()
-
-		// Store filename/path for returning and using later on
-		fpath := filepath.Join(dest, f.Name)
-		filenames = append(filenames, fpath)
-
-		if f.FileInfo().IsDir() {
-
-			os.MkdirAll(fpath, os.ModePerm)
-
-		} else {
-
-			var fdir string
-			if lastIndex := strings.LastIndex(fpath, string(os.PathSeparator)); lastIndex > -1 {
-				fdir = fpath[:lastIndex]
-			}
-
-			err = os.MkdirAll(fdir, os.ModePerm)
-			if err != nil {
-				log.Fatal(err)
-				return filenames, err
-			}
-			f, err := os.OpenFile(
-				fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-			if err != nil {
-				return filenames, err
-			}
-			defer f.Close()
-
-			_, err = io.Copy(f, rc)
-			if err != nil {
-				return filenames, err
-			}
-
-		}
-	}
-	return filenames, nil
-}
-
-func unzipFile(filename string, projectName string) {
-	files, err := Unzip(filename, projectName)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Unzipped: " + strings.Join(files, ", "))
+	return fmt.Sprintf("https://codeload.github.com/%s/zip/master", userURL)
 }
 
 func deleteFile(filename string) {
+	filename = ".\\" + filename
+	fmt.Println("Removing", filename)
 	os.Remove(filename)
 }
 
@@ -101,7 +36,7 @@ func downloadFile(projectName string, url string) {
 
 	io.Copy(out, response.Body)
 
-	unzipFile(filename, projectName)
+	archiver.Zip.Open(filename, projectName)
 	deleteFile(filename)
 }
 
@@ -117,7 +52,7 @@ func main() {
 	}
 
 	projectName := args[0]
-	url := getDownloadUrl(args[1])
+	url := getDownloadURL(args[1])
 
 	downloadFile(projectName, url)
 
