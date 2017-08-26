83ebd97f04141fa7ed3bda0fe1f9adb5c3c70881
Divan Visagie <visagiedivan@gmail.com>
2017-08-26 11:38:20 +0200

    do tar instead

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/mholt/archiver"
+	"path/filepath"
 )
 
 func getDownloadURL(userURL string) string {
 	// https://codeload.github.com/divanvisagie/postl/zip/master
-	return fmt.Sprintf("https://codeload.github.com/%s/zip/master", userURL)
+	return fmt.Sprintf("https://codeload.github.com/%s/tar.gz/master", userURL)
 }
 
 func deleteFile(filename string) {
@@ -21,12 +22,71 @@ func deleteFile(filename string) {
 	os.Remove(filename)
 }
 
+// Untar takes a destination path and a reader; a tar reader loops over the tarfile
+// creating the file structure at 'dst' along the way, and writing any files
+func Untar(dst string, r io.Reader) error {
+
+	gzr, err := gzip.NewReader(r)
+	defer gzr.Close()
+	if err != nil {
+		return err
+	}
+
+	tr := tar.NewReader(gzr)
+
+	for {
+		header, err := tr.Next()
+
+		switch {
+
+		// if no more files are found return
+		case err == io.EOF:
+			return nil
+
+		// return any other error
+		case err != nil:
+			return err
+
+		// if the header is nil, just skip it (not sure how this happens)
+		case header == nil:
+			continue
+		}
+
+		// the target location where the dir/file should be created
+		target := filepath.Join(dst, header.Name)
+
+		// the following switch could also be done using fi.Mode(), not sure if there
+		// a benefit of using one vs. the other.
+		// fi := header.FileInfo()
+
+		// check the file type
+		switch header.Typeflag {
+
+		// if its a dir and it doesn't exist create it
+		case tar.TypeDir:
+			if _, err := os.Stat(target); err != nil {
+				if err := os.MkdirAll(target, 0755); err != nil {
+					return err
+				}
+			}
+
+		// if it's a file create it
+		case tar.TypeReg:
+			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			// copy over contents
+			if _, err := io.Copy(f, tr); err != nil {
+				return err
+			}
+		}
+	}
+}
+
 func downloadFile(projectName string, url string) {
-
-	filename := projectName + ".zip"
-
-	out, _ := os.Create(filename)
-	defer out.Close()
 
 	response, getErr := http.Get(url)
 	if getErr != nil {
@@ -34,10 +94,8 @@ func downloadFile(projectName string, url string) {
 	}
 	defer response.Body.Close()
 
-	io.Copy(out, response.Body)
+	Untar(projectName, response.Body)
 
-	archiver.Zip.Open(filename, projectName)
-	deleteFile(filename)
 }
 
 func main() {
