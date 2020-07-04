63b8c1854dfb52565ba7dfa8b1d112670d69aa4f
Divan Visagie <me@dvisagie.com>
2020-07-04 14:51:53 +0200

    add requirements.txt

--- src/repo_test.py
+++ src/repo_test.py
@@ -2,7 +2,7 @@ import pytest
 
 from repo import complete_url
 
-def test_inserts_default_domain():
+def test_inserts_default_domain_given_github_short():
     actual = complete_url('divanvisagie/new')
     assert actual == 'git@github.com:divanvisagie/new.git'
 
