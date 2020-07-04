5c13013959358727ad8f460d7abeec6a04d7a9aa
Divan Visagie <me@dvisagie.com>
2020-07-04 21:33:43 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='new-project', # Replace with your own username
-    version='0.0.7',
+    version='0.0.8',
     author='Divan Visagie',
     author_email='me@dvisagie.com',
     description='Generate new projects from git repositories',
