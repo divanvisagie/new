34b314474e249225f307a6a1bf4f46a6f6df4deb
Divan Visagie <me@dvisagie.com>
2020-07-04 17:26:29 +0200

    Add input loop

--- src/template.py
+++ src/template.py
@@ -4,11 +4,13 @@ def get_input(prompt):
     return input(prompt)
 
 def enter_loop_with(config, prompt=get_input):
+    replacement_map = []
     for item in config['replace']['strings']:
         match = item['match']
         description = item['description']
-        prompt(f'Enter replacement text for:\n  {match}\nDescription:\n  {description}\n > ')
-
+        response = prompt(f'Enter replacement text for:\n  {match}\nDescription:\n  {description}\n > ')
+        replacement_map.append((match, response))
+    return replacement_map
 
 def read(project):
     # Prompts the user for any template replacement requirements
@@ -16,7 +18,7 @@ def read(project):
     try:
         with open(f'{project}/.new.yml') as file:
             f = yaml.load(file, Loader=yaml.FullLoader)
-            print(f)
+            enter_loop_with(f)
     except:
         print('no template config to process')
 
