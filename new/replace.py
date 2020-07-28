b2f34c3bc4f7c3c23a656182898a98a5e88c2847
Divan visagie <me@dvisagie.com>
2020-07-28 22:31:33 +0200

    some refactoring

--- new/replace.py
+++ new/replace.py
@@ -18,15 +18,23 @@ def name_in_line(line, name_to_find):
     (lout, confidence) = process.extractOne(name_to_find, split_lines)
     return lout.strip()
 
-def replace_strings(project, strings):
+def replace_strings_in_text(text_content, strings):
+    for pair in strings:
+        text_content = replace_match(pair, text_content)
+    return text_content
+
+def auto_replace(text_content, original_project_name):
+    pass
+
+def replace_strings(project, strings, original_project_name):
     for current_file_path in get_file_paths(project):
         try:
             with open(current_file_path, 'r+', encoding='utf-8') as f:
-                old = f.read() # read everything in the file
-                for pair in strings:
-                    old = replace_match(pair, old)
+                text_content = f.read() # read everything in the file
+                text_content = replace_strings_in_text(text_content,strings)
+                text_content = auto_replace(text_content, original_project_name)
                 f.seek(0) # go to the beginning of the file
-                f.write(old) # write the new line before
+                f.write(text_content) # write the new line before
                 f.truncate() # cut off any remaining text from the old file
         except UnicodeDecodeError:
             pass # Probably non text data, we can skip it
